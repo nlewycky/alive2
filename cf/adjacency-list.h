3cab4ccd03d7b21c7e58af2e449d72f705c209c2
Nick Lewycky <nicholas@mxc.ca>
2022-06-23 19:26:01 -0700

    Initial commit of ConstantFold auto-generation.

---
+++ cf/adjacency-list.h
@@ -0,0 +1,244 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <tuple>
+#include <vector>
+
+namespace {
+struct SimpleRootedDigraphHash;
+
+// A rooted simple directed graph.
+//
+// The root node is the first node, and there are no edges to that node.
+class SimpleRootedDigraph {
+public:
+  friend struct SimpleRootedDigraphHash;
+
+  // The out-degrees are listed as a closed range with INT_MAX meaning infinity.
+  // So an edge with 2 edges out is [2, 2] while an edge with 2 or more is
+  // [2, INT_MAX] and an edge with zero through 5 is [0, 5].
+  explicit SimpleRootedDigraph(
+      std::vector<std::tuple<uint8_t, uint8_t>> &out_degree_)
+      : out_degree_(out_degree_) {
+    const int size = out_degree_.size();
+    for (auto [minlen, maxlen] : out_degree_) {
+      assert(minlen <= maxlen);
+      assert(minlen <= size);
+      if (maxlen > size) {
+        maxlen = size;
+      }
+      adjacency_list.emplace_back(std::vector<uint8_t>());
+      if (maxlen < minlen * 2) {
+        adjacency_list.back().reserve(maxlen);
+      } else {
+        adjacency_list.back().reserve(minlen);
+      }
+    }
+    assert(out_degree_.size() == adjacency_list.size());
+  }
+
+  ~SimpleRootedDigraph() = default;
+  SimpleRootedDigraph(const SimpleRootedDigraph &) = default;
+  SimpleRootedDigraph(SimpleRootedDigraph &&) noexcept = default;
+  SimpleRootedDigraph &operator=(const SimpleRootedDigraph &other) {
+    // assert that out_degree_ == other.out_degree_.
+    adjacency_list = other.adjacency_list;
+    return *this;
+  }
+
+  bool operator==(const SimpleRootedDigraph &other) const {
+    assert(adjacency_list.size() == other.adjacency_list.size());
+    for (int i = 0, e = adjacency_list.size(); i != e; ++i) {
+      if (adjacency_list[i].size() < other.adjacency_list[i].size())
+        return false;
+      // Note: we use an O(n^2) algorithm here because it's faster on small
+      // graphs. The alternative is to sort adjacency_list[i] and
+      // other.adjacency_list[i] O(n log n) and use linear time comparison.
+      for (auto lhs : adjacency_list[i]) {
+        if (std::find(other.adjacency_list[i].begin(),
+                      other.adjacency_list[i].end(),
+                      lhs) == other.adjacency_list[i].end())
+          return false;
+      }
+      /*
+      // untested O(n log n) implementation:
+      auto lhs = adjacency_list[i];
+      auto rhs = other.adjacency_list[i];
+      std::sort(lhs.begin(), lhs.end());
+      std::sort(rhs.begin(), rhs.end());
+      if (lhs != rhs)
+        return false;
+      */
+    }
+    return true;
+  }
+
+  bool operator!=(const SimpleRootedDigraph &other) const {
+    return !this->operator==(other);
+  }
+
+  void add_edge(uint8_t from, uint8_t to) {
+    assert(to != 0);
+    assert(edge(from, to) == false);
+    assert(adjacency_list[from].size() < std::get<1>(out_degree_[from]));
+    adjacency_list[from].push_back(to);
+  }
+
+  bool edge(uint8_t from, uint8_t to) const {
+    assert(from != to);
+    assert(from < adjacency_list.size());
+    assert(to < adjacency_list.size());
+    return std::find(adjacency_list[from].begin(), adjacency_list[from].end(),
+                     to) != adjacency_list[from].end();
+  }
+
+  uint8_t out_degree(uint8_t node) const {
+    assert(node < adjacency_list.size());
+    return adjacency_list[node].size();
+  }
+
+  const std::tuple<uint8_t, uint8_t> &out_degree_spec(uint8_t node) const {
+    assert(node < out_degree_.size());
+    return out_degree_[node];
+  }
+
+  bool all_reachable() const {
+    // "visited" indicates either we've added its adjancencies or we've added
+    // it to the node_stack.
+    std::vector<bool> visited(adjacency_list.size(), false);
+    visited[0] = true;
+    std::vector<int> node_stack;
+    node_stack.emplace_back(0);
+    do {
+      auto node = node_stack.back();
+      node_stack.pop_back();
+
+      for (int adjacent : adjacency_list[node]) {
+        if (!visited[adjacent]) {
+          visited[adjacent] = true;
+          node_stack.emplace_back(adjacent);
+        }
+      }
+    } while (!node_stack.empty());
+
+    for (bool b : visited) {
+      if (!b)
+        return false;
+    }
+    return true;
+  }
+
+  // Check whether this graph contains a cycle. This data structure is never
+  // supposed to contain cycles, so this should only be used to verify that
+  // assertion. If you want to know whether there's a cycle outside debugging,
+  // the answer is 'no'.
+  bool is_acyclic() const {
+#ifndef NDEBUG
+    std::vector<bool> visited(adjacency_list.size(), false);
+    std::vector<std::pair<int, int>> node_stack;
+    node_stack.reserve(adjacency_list.size());
+    node_stack.emplace_back(0, 1);
+    do {
+      auto [node, adjacency_index] = node_stack.back();
+      node_stack.pop_back();
+      visited[node] = true;
+
+      int out_degree_count = out_degree(node);
+      for (int i = adjacency_index; i < out_degree_count; ++i) {
+        int a = adjacency_list[node][i];
+        assert(edge(node, a));
+        if (visited[a]) {
+          // Cycle found.
+          return false;
+        }
+        // Save our current state in our current node.
+        if (i != out_degree_count - 1)
+          node_stack.emplace_back(node, i + 1);
+        // Depth first visit, switch to child node now.
+        node_stack.emplace_back(a, 1);
+        break;
+      }
+    } while (!node_stack.empty());
+#endif
+    return true;
+  }
+
+  bool has_path(uint8_t from, uint8_t to) const {
+    assert(to != 0);
+    assert(from < adjacency_list.size());
+    assert(to < adjacency_list.size());
+    std::vector<bool> visited(adjacency_list.size(), false);
+    std::vector<int> worklist;
+    worklist.reserve(adjacency_list.size());
+    worklist.emplace_back(from);
+    do {
+      int node = worklist.back();
+      worklist.pop_back();
+      for (int adjacency : adjacency_list[node]) {
+        if (adjacency == to)
+          return true;
+        if (visited[adjacency])
+          continue;
+        worklist.push_back(adjacency);
+        visited[adjacency] = true;
+      }
+    } while (!worklist.empty());
+    return false;
+  }
+
+  unsigned size() const {
+    return adjacency_list.size();
+  }
+
+  const std::vector<std::vector<uint8_t>> &graph() const {
+    return adjacency_list;
+  }
+
+  void dump() const {
+    for (int i = 0, e = adjacency_list.size(); i < e; ++i) {
+      for (auto a : adjacency_list[i])
+        printf("n%d -> n%d\n", i, a);
+    }
+    printf("\n");
+  }
+
+private:
+  //const std::vector<std::tuple<uint8_t, uint8_t>> out_degree_;
+  const std::vector<std::tuple<uint8_t, uint8_t>> &out_degree_;
+
+  std::vector<std::vector<uint8_t>> adjacency_list;
+};
+
+struct SimpleRootedDigraphHash {
+  uint32_t operator()(SimpleRootedDigraph const &g) const noexcept {
+    uint32_t hash = 0;
+    for (int x = 0, xe = g.adjacency_list.size(); x != xe; ++x) {
+      hash = hash_combine(lowbias32(x), hash);
+      for (const auto &y : g.adjacency_list[x]) {
+        hash = hash_combine(lowbias32(y), hash);
+      }
+    }
+    return hash;
+  }
+
+private:
+  // https://github.com/skeeto/hash-prospector
+  static uint32_t lowbias32(uint32_t x) {
+    x ^= x >> 16;
+    x *= UINT32_C(0x7feb352d);
+    x ^= x >> 15;
+    x *= UINT32_C(0x846ca68b);
+    x ^= x >> 16;
+    return x;
+  }
+
+  // https://stackoverflow.com/questions/2590677/how-do-i-combine-hash-values-in-c0x
+  static uint32_t hash_combine(uint32_t x, uint32_t y) {
+    return y + 0x9e3779b9 + (x << 6) + (x >> 2);
+  }
+};
+
+} // namespace
---
+++ cf/cf.cpp
@@ -0,0 +1,326 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <map>
+#include <optional>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "llvm_util/llvm2alive.h"
+#include "llvm_util/utils.h"
+
+#include "adjacency-list.h"
+
+static const int size = 12;
+
+namespace {
+
+template <typename T, T First, T Last> bool advance(std::vector<T> &ops) {
+  int n = ops.size();
+  int last = n - 1;
+  for (int i = 0; i < n; ++i) {
+    if (ops[i] < Last) {
+      ++ops[i];
+      return true;
+    }
+    assert(ops[i] == Last);
+    if (i == last)
+      return false;
+    ops[i] = First;
+  }
+  return true;
+}
+
+typedef uint8_t Ops;
+static const Ops FirstOp = 0;
+static const Ops Add = 0;
+static const Ops Sub = 1;
+static const Ops Mul = 2;
+static const Ops UDiv = 3;
+static const Ops SDiv = 4;
+static const Ops URem = 5;
+static const Ops SRem = 6;
+static const Ops Shl = 7;
+static const Ops LShr = 8;
+static const Ops AShr = 9;
+static const Ops And = 10;
+static const Ops Or = 11;
+static const Ops Xor = 12;
+static const Ops LastOp = 12;
+
+// Out points towards arguments/inputs to the function.
+unsigned out_edges(Ops op) {
+  switch (op) {
+  case Add:
+  case Sub:
+  case Mul:
+  case UDiv:
+  case SDiv:
+  case URem:
+  case SRem:
+  case Shl:
+  case LShr:
+  case AShr:
+  case And:
+  case Or:
+  case Xor:
+    return 2;
+  }
+  std::abort();
+}
+
+// We implement our own evaluator so we can burn through all the obvious
+// non-matches quickly and without involving memory allocation before creating
+// LLVM IR and sending them to alive.
+constexpr int eval_max = 0x7ff;
+int eval_op(Ops op, const std::vector<int> &v) {
+  assert(v.size() == 2);
+  switch (op) {
+  case Add:
+    return ((unsigned)v[0] + (unsigned)v[1]) & eval_max;
+  case Sub:
+    return ((unsigned)v[0] - (unsigned)v[1]) & eval_max;
+  case Mul:
+    return ((unsigned)v[0] * (unsigned)v[1]) & eval_max;
+  case UDiv:
+    if (v[1] == 0) return eval_max - 1;
+    return ((unsigned)v[0] / (unsigned)v[1]) & eval_max;
+  case SDiv:
+    if (v[1] == 0) return eval_max - 2;
+    if (v[0] == INT_MIN && v[1] == -1) return eval_max - 3;
+    return ((signed)v[0] / (signed)v[1]) & eval_max;
+  case URem:
+    if (v[1] == 0) return eval_max - 4;
+    return ((unsigned)v[0] % (unsigned)v[1]) & eval_max;
+  case SRem:
+    if (v[1] == 0) return eval_max - 5;
+    if (v[0] == INT_MIN && v[1] == -1) return eval_max - 6;
+    return ((signed)v[0] % (signed)v[1]) & eval_max;
+  case Shl:
+    return ((unsigned)v[0] << (unsigned)v[1]) & eval_max;
+  case LShr:
+    return (unsigned)v[0] << (unsigned)v[1];
+  case AShr:
+    if ((unsigned)v[1] > sizeof(signed)) return eval_max - 7;
+    return ((signed)v[0] << (signed)v[1]) & eval_max;
+  case And:
+    return v[0] & v[1];
+  case Or:
+    return v[0] | v[1];
+  case Xor:
+    return v[0] ^ v[1];
+  }
+  std::abort();
+}
+
+int eval(const SimpleRootedDigraph &g, const std::vector<Ops> &ops) {
+  assert(g.size() == ops.size());
+  assert(g.all_reachable());
+  assert(g.is_acyclic());
+
+  int generated_value = 3;
+  auto x = g.graph();
+  std::vector<std::pair<int, int>> node_stack;
+  std::vector<std::optional<int>> values;
+  values.resize(g.size());
+  node_stack.emplace_back(0, 0);
+  do {
+  top:
+    auto &[node, adj_i] = node_stack.back();
+    assert(!values[node].has_value());
+    auto &adj_list = x[node];
+    for (int adj_list_length = adj_list.size(); adj_i != adj_list_length;
+         ++adj_i) {
+      if (!values[adj_list[adj_i]].has_value()) {
+        // Stop, proceed to the child node now.
+        node_stack.emplace_back(adj_list[adj_i], 0);
+        goto top;
+      }
+    }
+    std::vector<int> adj_values;
+    adj_values.reserve(out_edges(ops[node]));
+    for (auto adj : adj_list) {
+      assert(values[adj].has_value());
+      adj_values.emplace_back(values[adj].value());
+    }
+    while (adj_values.size() < out_edges(ops[node])) {
+      adj_values.push_back(generated_value);
+      ++generated_value;
+    }
+    values[node] = eval_op(ops[node], adj_values);
+    // printf("node %d = %d\n", node, values[node].value());
+    node_stack.pop_back(); // kills 'node' and 'adj_i'
+  } while (!values[0]);
+  return values[0].value();
+}
+
+}
+
+int main(void) {
+  /*
+  llvm::LLVMContext C;
+  llvm::FunctionType *FTy = FunctionType::get(Type::getVoidType(C), 0);
+
+  llvm::Module M("", C);
+  llvm::Function *F = llvm::cast<Function>(M.getOrInsertFunction("", FTy));
+  llvm::
+  */
+
+  // We want to efficiently generate every possible DAG of expressions on the
+  // operators we want to fold.
+  //
+  // To make this tractable, we order the search in a specific way:
+  // - The actual graph contains only the operations, none of the variables.
+  //   Variables have no out-edges and are never shared. If you want to know
+  //   whether op(x, x) folds, use op(x, y) /\ x == y.
+  // - We can exploit the fact that our nodes have an out-degree known in
+  //   advance. The `add` instruction takes two arguments, never one or three.
+  //   There's no need to try to create a node with three out-edges when none of
+  //   our operations have three out-edges.
+  // - That's why it's more efficient to use an adjacency list representation.
+  //   Each node has a single row with a maximum number of out-edges (the
+  //   "out-degree-spec") to other nodes.
+  // - A nodes out edges are ordered, as not all operations are commutable. The
+  //   adjacency list entries 'node 1: [2, 3]' and 'node 1: [3, 2]' are
+  //   distinct.
+  // - We grow the graph by trying to add one edge at a time, starting from then
+  //   empty graph.
+  //   * Once a graph has a cycle, adding more edges can never break the cycle.
+  //     If the graph has a path a -> b, then we don't try adding edge b -> a.
+  //   * We want our graph to use all its nodes. Not using all the nodes implies
+  //     that it's equivalen to a smaller graph that we would have explored
+  //     in a previous iteration.
+  // - We have a hash function over the graphs and use it to avoid duplicates.
+  // - Once a graph is created, we try out every possible assignment over the
+  //   nodes, based on what is valid for the out-degree of the node. Recall
+  //   that we don't include variables as nodes in our graph, so an 'add' which
+  //   requires an out-degree of 2, fits a node with out-degree of 0, 1, or 2.
+  // - We now have a graph with an assignment of operators to nodes.
+
+  std::vector<std::tuple<uint8_t, uint8_t>> out_degree;
+  for (int i = 0; i < size; ++i) {
+    out_degree.emplace_back(UINT8_C(2), UINT8_C(2));
+  }
+
+  std::vector<SimpleRootedDigraph> v;
+  v.emplace_back(SimpleRootedDigraph(out_degree));
+
+  int count = 0;
+#ifndef NDEBUG
+  std::map<int, SimpleRootedDigraph> hashes;
+#else
+  std::set<int> hashes;
+#endif
+  do {
+    std::set<uint32_t> v2_hashes;
+    std::vector<SimpleRootedDigraph> v2;
+    for (const auto &g : v) {
+      for (int i = 0; i < size; ++i) {
+        auto [min_out_degree, max_out_degree] = g.out_degree_spec(i);
+        int out_degree = g.out_degree(i);
+        if (out_degree == max_out_degree)
+          continue;
+        for (int j = 1; j < size; ++j) {
+          if (i == j)
+            continue;
+          if (g.edge(i, j))
+            continue;
+          if (i == 0 || !g.has_path(j, i)) {
+            SimpleRootedDigraph g2(g);
+            g2.add_edge(i, j);
+            assert(g2.is_acyclic());
+            uint32_t hash = SimpleRootedDigraphHash{}(g2);
+            auto [it, did_insert] = v2_hashes.insert(hash);
+            if (did_insert) {
+              v2.push_back(g2);
+              if (g2.all_reachable()) {
+#ifndef NDEBUG
+                auto [it, did_insert] = hashes.insert({hash, v2.back()});
+                if (!did_insert) {
+                  if (it->second != v2.back()) {
+                    printf("Same hash=%xu but inequal graphs:\n", hash);
+                    it->second.dump();
+                    v2.back().dump();
+                    std::abort();
+                  }
+                } else {
+                  ++count;
+                }
+#else
+                auto [it, did_insert] = hashes.insert(hash);
+                if (did_insert)
+                  ++count;
+#endif
+              }
+            }
+          }
+        }
+      }
+    }
+    std::swap(v, v2);
+  } while (!v.empty());
+
+  std::vector<Ops> ops;
+  for (int i = 0; i < size; ++i) {
+    ops.push_back(Add);
+  }
+
+  // After assigning operators we can determine how many free variables there
+  // are on the expression. We group our expressions based on how many free
+  // variables they have. They can't be equal to each other unless one of them
+  // isn't using all its variables, which we scan for explicitly.
+
+  // buckets[free_variable_count][eval_result][:]<0=graph, 1=ops>
+  std::vector<std::vector<
+      std::vector<std::tuple<SimpleRootedDigraph, std::vector<Ops>>>>>
+      buckets(
+          size * size,
+          std::vector<
+              std::vector<std::tuple<SimpleRootedDigraph, std::vector<Ops>>>>(
+              eval_max + 1)); // TODO: this wastes a little memory.
+
+  for (const auto &[hash, graph] : hashes) {
+    std::vector<Ops> ops(size, FirstOp);
+    int free_variable_count = 0;
+    for (int i = 0; i < size; ++i) {
+      free_variable_count +=
+          std::get<0>(graph.out_degree_spec(i)) - graph.out_degree(i);
+    }
+    do {
+      int bucket = eval(graph, ops);
+      buckets[free_variable_count][bucket].emplace_back(graph, ops);
+    } while (advance<Ops, FirstOp, LastOp>(ops));
+  }
+
+  for (int free_variable_count = 0, fe = buckets.size();
+       free_variable_count != fe; ++free_variable_count) {
+    if (buckets[free_variable_count].empty())
+      continue;
+    for (int i = 0; i != eval_max; ++i) {
+      if (buckets[free_variable_count][i].size() < 2)
+        continue;
+      // TODO: All these [graph, ops] are probably equivalent. Test them!
+      //for (const auto &[graph, ops] : buckets[free_variable_count][i]) {
+      //  emit_llvm_ir...
+      //  llvm2alive...
+      //  use alive-tv to compare 0 to 1, 1 to 2, ... n-1 to n, n to 0.
+      //  if necessary, form new equivalence classes
+      //}
+    }
+  }
+
+  // TODO: ...
+  // Given a graph with an assignment of operators to nodes, we can check
+  // whether there are 'magic' constants for some of the variables which cause
+  // the output to be constant.
+  // - Graphs with different number of free variables are never equal, so we
+  //   divide them up that way first.
+
+  // TODO: ...
+  // Given a graph with an assignment of operators to nodes, we can check
+  // whether there are 'magic' constants for some of the variables which cause
+  // the output to be one of the other variables.
+}
